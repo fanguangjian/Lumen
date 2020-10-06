7dd457e720ff48938353cdb1ed7d4f35c00da319
Grant <fanguangjianauto@gmail.com>
2020-10-07 00:31:12 +1100

    Error handler

--- Udemy-Lumen/app/Exceptions/Handler.php
+++ Udemy-Lumen/app/Exceptions/Handler.php
@@ -2,8 +2,12 @@
 
 namespace App\Exceptions;
 
+use App\Traits\ApiResponder;
+use http\Env\Response;
 use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Validation\ValidationException;
 use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
 use Symfony\Component\HttpKernel\Exception\HttpException;
@@ -11,6 +15,7 @@ use Throwable;
 
 class Handler extends ExceptionHandler
 {
+    use ApiResponder;
     /**
      * A list of the exception types that should not be reported.
      *
@@ -47,8 +52,36 @@ class Handler extends ExceptionHandler
      *
      * @throws \Throwable
      */
+     //    错误处理
+
     public function render($request, Throwable $exception)
     {
-        return parent::render($request, $exception);
+        if ($exception instanceof HttpException){
+            $code = $exception -> getStatusCode();
+            $message = Response::$statusTexts[$code];
+            return $this->errorResponse($message, $code);
+        }
+        if ($exception instanceof  ModelNotFoundException) {
+            $model = strtolower(class_basename($exception->getModel()));
+            return $this->errorResponse("Does not exist any instance of {$model} with the given id", Response::HTTP_NOT_FOUND);
+        }
+        if ($exception instanceof  AuthorizationException) {
+            return $this->errorResponse($exception->getMessage(), Response::HTTP_FORBIDDEN);
+        }
+        if ($exception instanceof  AuthenticationException) {
+            return $this->errorResponse($exception->getMessage(), Response::HTTP_UNAUTHORIZED);
+        }
+        if ($exception instanceof  ValidationException) {
+            $errors = $exception->validator->errors()->getMessages();
+//            注意
+//            return $this->errorResponse($errors, Response::HTTP_UNPROCESSABLE_ENTITY);
+            return $this->errorResponse($errors, \Illuminate\Http\Response::HTTP_UNPROCESSABLE_ENTITY);
+
+        }
+        if (env('APP_DEBUG', false)){
+            return parent::render($request, $exception);
+        }
+
+        return $this->errorResponse('Unexpected error, try later',Response::HTTP_INTERNAL_SERVER_ERROR);
     }
 }
