a030812aeadffdc79d99812798959b9438a8562d
Grant <fanguangjianauto@gmail.com>
2020-10-24 00:37:48 +1100

    Book controller get books

--- LumenBookAPI/app/Http/Controllers/BookController.php
+++ LumenBookAPI/app/Http/Controllers/BookController.php
@@ -3,9 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Book;
+use App\Traits\ApiResponser;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use App\Traits\ApiResponser;
 
 
 class BookController extends Controller
@@ -24,33 +24,35 @@ class BookController extends Controller
 
     public function index()
     {
-
+        $books = Book::all();
+//        return $this->successResponse($books);
+        return $this->successResponse($books);
     }
 
     /**
      * @Notes: 添加数据
      */
-    public function store(Request $request)
-    {
-
-    }
-
-    /**
-     * @Notes:  根据Id查询一条
-     * @Interface show
-     */
-    public function show($book)
-    {
-
-    }
-
-    public function update(Request  $request, $book)
-    {
-
-    }
-
-    public function destroy($book)
-    {
-
-    }
+//    public function store(Request $request)
+//    {
+//
+//    }
+//
+//    /**
+//     * @Notes:  根据Id查询一条
+//     * @Interface show
+//     */
+//    public function show($book)
+//    {
+//
+//    }
+//
+//    public function update(Request  $request, $book)
+//    {
+//
+//    }
+//
+//    public function destroy($book)
+//    {
+//
+//    }
 }
