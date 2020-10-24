7d0baed2a6ae71b983b8f3493c0879500b24cddb
Grant <fanguangjianauto@gmail.com>
2020-10-25 00:32:44 +1100

    Book controller store, update, show, remove

--- LumenBookAPI/app/Http/Controllers/BookController.php
+++ LumenBookAPI/app/Http/Controllers/BookController.php
@@ -6,7 +6,7 @@ use App\Book;
 use App\Traits\ApiResponser;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-
+use Illuminate\Validation\ValidationException;
 
 class BookController extends Controller
 {
@@ -32,27 +32,60 @@ class BookController extends Controller
     /**
      * @Notes: 添加数据
      */
-//    public function store(Request $request)
-//    {
-//
-//    }
-//
-//    /**
-//     * @Notes:  根据Id查询一条
-//     * @Interface show
-//     */
-//    public function show($book)
-//    {
-//
-//    }
-//
-//    public function update(Request  $request, $book)
-//    {
-//
-//    }
-//
-//    public function destroy($book)
-//    {
-//
-//    }
+    public function store(Request $request)
+    {
+        $rules = [
+            'title' => 'required|max:255',
+            'description' => 'required|max:255',
+            'price' => 'required|min:1',
+            'author_id' => 'required|min:1',
+        ];
+        $this->validate($request, $rules);
+        $book = Book::create($request->all());
+        return $this->successResponse($book, Response::HTTP_CREATED);
+
+    }
+
+    /**
+     * @Notes:  根据Id查询一条
+     * @Interface show
+     */
+    public function show($book)
+    {
+//        $author = Author::findOrFail($author);
+        $book = Book::find($book);
+        return $this->successResponse($book);
+
+    }
+
+    public function update(Request  $request, $book)
+    {
+        $rules = [
+            'title' => 'required|max:255',
+            'description' => 'required|max:255',
+//            'price' => 'min:1',
+//            'author_id' => 'min:1',
+            'price' => 'required|min:1',
+            'author_id' => 'required|min:1',
+        ];
+        $this->validate($request, $rules);
+        $book = Book::findOrFail($book);
+        $book->fill($request->all());
+        if($book->isClean()){
+            return $this->errorResponse('At least one value must change', Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+        $book->save();
+        return $this->successResponse($book);
+
+    }
+
+    public function destroy($book)
+    {
+        $book = Book::findOrFail($book);
+        $book->delete();
+        return $this->successResponse($book);
+
+    }
+
+
 }
