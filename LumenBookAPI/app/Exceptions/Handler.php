18cd5d8979f2d4f4d844f9e809f72966fa3016cd
Grant <fanguangjianauto@gmail.com>
2020-11-09 23:34:07 +1100

    BookApi Handle Error, error with  "ext-http": "*"

--- LumenBookAPI/app/Exceptions/Handler.php
+++ LumenBookAPI/app/Exceptions/Handler.php
@@ -3,7 +3,9 @@
 namespace App\Exceptions;
 
 use Exception;
+use http\Env\Response;
 use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Validation\ValidationException;
 use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
@@ -46,5 +48,32 @@ class Handler extends ExceptionHandler
     public function render($request, Exception $exception)
     {
         return parent::render($request, $exception);
+//        if ($exception instanceof HttpException){
+//            $code = $exception -> getStatusCode();
+//            $message = Response::$statusTexts[$code];
+//            return $this->errorResponse($message, $code);
+//        }
+//        if ($exception instanceof  ModelNotFoundException) {
+//            $model = strtolower(class_basename($exception->getModel()));
+//            return $this->errorResponse("Does not exist any instance of {$model} with the given id", Response::HTTP_NOT_FOUND);
+//        }
+//        if ($exception instanceof  AuthorizationException) {
+//            return $this->errorResponse($exception->getMessage(), Response::HTTP_FORBIDDEN);
+//        }
+//        if ($exception instanceof  AuthenticationException) {
+//            return $this->errorResponse($exception->getMessage(), Response::HTTP_UNAUTHORIZED);
+//        }
+//        if ($exception instanceof  ValidationException) {
+//            $errors = $exception->validator->errors()->getMessages();
+////            注意
+////            return $this->errorResponse($errors, Response::HTTP_UNPROCESSABLE_ENTITY);
+//            return $this->errorResponse($errors, \Illuminate\Http\Response::HTTP_UNPROCESSABLE_ENTITY);
+//
+//        }
+//        if (env('APP_DEBUG', false)){
+//            return parent::render($request, $exception);
+//        }
+//
+//        return $this->errorResponse('Unexpected error, try later',Response::HTTP_INTERNAL_SERVER_ERROR);
     }
 }
