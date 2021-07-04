198fa099b769e5125b63adc5cf533cda81c16bf4
Grant <fanguangjianauto@gmail.com>
2021-07-04 23:29:16 +1000

    Add  book service

--- lumenGateway5.7/app/Http/Controllers/BookController.php
+++ lumenGateway5.7/app/Http/Controllers/BookController.php
@@ -3,77 +3,75 @@
 namespace App\Http\Controllers;
 
 use App\Book;
-use App\Services\BookService;
 use App\Traits\ApiResponser;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use Illuminate\Validation\ValidationException;
-use App\Services\AuthorService;
-
+use App\Services\BookService;
 
 class BookController extends Controller
 {
     use ApiResponser;
-    public $bookService;
-    public $authorService;
 
+    /**
+     * The service to consume the authors microservice
+     * @var bookService
+     */
+    public $bookService;
 
     /**
      * Create a new controller instance.
      *
      * @return void
      */
-    public function __construct(BookService $bookService, AuthorService $authorService)
+    public function __construct(BookService $bookService)
     {
         $this->bookService = $bookService;
-        $this->authorService = $authorService;
-    }
-
-    public function index()
-    {
-//        $books = Book::all();
-////        return $this->successResponse($books);
-//        return $this->successResponse($books);
-
-
-        return $this->successResponse(
-            $this->bookService->obtainBooks()
-        );
-
-
-//        return "AAA";
-
     }
 
     /**
-     * @Notes: 添加数据
+     * Return the list of authors
+     * @return Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return $this->successResponse($this->bookService->obtainBooks());
+//        return  "AAAA";
+    }
+
+
+    /**
+     * Create one new author
+     * @return Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-
-
+        return $this->successResponse($this->bookService->createBook($request->all(), Response::HTTP_CREATED));
     }
 
     /**
-     * @Notes:  根据Id查询一条
-     * @Interface show
+     * Obtains and show one author
+     * @return Illuminate\Http\Response
      */
     public function show($book)
     {
-
+        return $this->successResponse($this->bookService->obtainBook($book));
     }
 
-    public function update(Request  $request, $book)
+    /**
+     * Update an existing author
+     * @return Illuminate\Http\Response
+     */
+    public function update(Request $request, $book)
     {
-
-
+        return $this->successResponse($this->bookService->editBook($request->all(), $book));
     }
 
+    /**
+     * Remove an existing author
+     * @return Illuminate\Http\Response
+     */
     public function destroy($book)
     {
-
-
+        return $this->successResponse($this->bookService->deleteBook($book));
     }
-
-
 }
